2584c0ead775a187c3e2605636e3147fdd6ef852
alu4543 <simotenerife@hotmail.com>
2012-11-29 20:46:40 +0000

    Se debe poder sumar dos matrices de racionales

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -7,6 +7,20 @@ describe Matrix do
     @matrixnumeric_1 = Matrix.new([[1,1],[2,2]])
     @matrixnumeric_2 = Matrix.new([[0,1],[1,1]])
 
+    medio = Racional.new(1,2)
+    tercio = Racional.new(1,3)
+    cuarto = Racional.new(1,4)
+    quinto = Racional.new(1,5)
+
+    
+    dosmedio = Racional.new(2,2)
+    dostercios = Racional.new(2,3)
+    doscuartos = Racional.new(2,4)
+    dosquintos = Racional.new(2,5)
+
+    @matrixracional_1 = Matrix.new([[medio,tercio],[[cuarto,quinto]])
+    @matrixracional_2 = Matrix.new([[dosmedio,dostercios],[[doscuartos,dosquintos]])
+
   end
 
   it "Se debe poder sumar dos matrices de enteros" do
@@ -17,5 +31,7 @@ describe Matrix do
     (@matrixnumeric_1 + @matrixnumeric_2).matrix.inspect.should == "[[0, 1], [2, 2]]"
   end
 
-
+  it "Se debe poder sumar dos matrices de racionales" do
+    (@matrixracional_1 + @matrixracional_2).racional.should == "[[3/4, 1/1], [3/4, 3/5]]"
+  end
 end
